95dae50d31237ace4ba91c7ea37eecc1a30d0091
nallaajaykrishnareddy <ajaykrish.nalla@gmail.com>
2024-08-27 17:34:05 +0530

    find duplicates

--- arrays/findDuplicate.js
+++ arrays/findDuplicate.js
@@ -1,3 +1,14 @@
-const findDuplicate = (nums) => {};
+const findDuplicate = (nums) => {
+  const set = new Set();
+  const duplicates = [];
+  for (const num of nums) {
+    if (set.has(num) && duplicates.includes(num)) {
+      duplicates.push(num);
+    } else {
+      set.add(num);
+    }
+  }
+  return duplicates;
+};
 
 console.log(findDuplicate([1, 3, 4, 2, 2]));
